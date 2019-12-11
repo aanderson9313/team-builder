8e1f5ccc3ea3b27365061ebd6ba2e4ee5f8577f9
alexis-anderson <aanderson9313@gmail.com>
2019-12-10 23:28:38 -0500

    finished mvp

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Form from './Components/Form';
+import Members from './Components/Members';
+import data from './data';
 import './App.css';
 
 function App() {
+  const [members, setMembers] = useState(data)
+
+  const addNewMember = member => {
+    setMembers([ ...members, member])
+
+  }
   return (
     <div className="App">
       <header className="App-header">
+        <h2>Member List </h2>
+        <div className = 'Form-Box'>
+        <Form addNewMember = {addNewMember} />
+        <Members memberList = {members} />
+         
+        </div>
      
       </header> 
     </div>
---
+++ src/data.js
@@ -0,0 +1,8 @@
+export default [
+    {
+        name: 'Alexis Anderson',
+        email: 'aanderson9313@gmail.com',
+        role: 'Full-Stack Web Developer',
+
+    },
+]
